49446df0d1f9a6fbd17902923437a00401ee4a37
sunny-utility-4827 <mzakzook@me.com>
2019-08-15 18:54:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,6 @@ function iLoveTheBeatles(number) {
   loveBeatles.push('I love the Beatles!');
   number++;
   }
-  while (number < 15 && loveBeatles.length < (15 - number));
+  while (number < 15 && loveBeatles.length <= (15 - number));
   return loveBeatles
 }
